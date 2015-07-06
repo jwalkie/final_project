a8e1da64222dfd339e740262ec5218794c2183dd
emt2cy <emt2cy@virginia.edu>
2015-07-06 11:03:17 +0200

    made welcome controller and corresponding index/show views. Created /learnmore route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  
+  get '/learnmore' => "welcome#show"
   root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
