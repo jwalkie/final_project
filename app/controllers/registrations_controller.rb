6467be80a47d69d6e9d20a205a1075cdb6488bcc
emt2cy <emt2cy@virginia.edu>
2015-07-07 19:40:39 +0200

    added some navigation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(calculators)
-    reports_path
+    new_report_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707144202) do
+ActiveRecord::Schema.define(version: 20150707172103) do
 
   create_table "foods", force: :cascade do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150707144202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.date     "date"
   end
 
   create_table "users", force: :cascade do |t|
